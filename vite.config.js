260d279a27a3234a4daec7ae56fba4335f6276e5
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-04-18 10:36:44 -0400

    Name collision with exports and typings

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'packages/vicuna-7b/vicuna7b.ts'),
+      entry: path.resolve(__dirname, 'packages/vicuna-7b/index.ts'),
       name: 'Vicuna7B',
       fileName: (format) => `Vicuna7B.${format}.js`
     }
